f66c01c8bab55f61a314fab54008db2fb7bf0245
eldad-assaf <eldadux@gmail.com>
2023-08-28 15:32:43 +0300

    sync

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,22 +1,30 @@
-const Product =require('../models/product')
-
-
+const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price: 25 })
-  res.status(200).json({products , nbHits: products.length})
-  console.log(products)
+  const products = await Product.find({ price: 25 });
+  res.status(200).json({ products, nbHits: products.length });
+  console.log(products);
 };
 
 const getAllProducts = async (req, res) => {
   console.log(req.query);
-  const products = await Product.find( req.query)
-  res.status(200).json({ products,  nbHits: products.length });
+//**trys to deconstruct the 'featured' parm from the query  */
+  const { featured ,company} = req.query;
+
+  const queryObject = {};
+  //**If 'featured' exists then set the right value in the 'queryObject' */
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  if(company){
+    queryObject.company = company
+  }
+  console.log(queryObject);
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
-const createProduct = async (req, res) => {
- 
+const createProduct = async (req, res) => {};
 
-  };
-
-module.exports = { getAllProductsStatic, getAllProducts,createProduct };
+module.exports = { getAllProductsStatic, getAllProducts, createProduct };
