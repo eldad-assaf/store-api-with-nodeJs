0e421f29857feed7684a209e10d8091a80cf483c
eldad-assaf <eldadux@gmail.com>
2023-08-28 20:14:47 +0300

    sync

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,15 +1,15 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ price: 25 });
+  const products = await Product.find({}).select("name price").limit(10);
   res.status(200).json({ products, nbHits: products.length });
   console.log(products);
 };
 
 const getAllProducts = async (req, res) => {
-  console.log(req.query);
-//**trys to deconstruct the 'featured' parm from the query  */
-  const { featured ,company} = req.query;
+  //console.log(req.query);
+  //**trys to deconstruct the 'featured', 'company' ... parms from the query  */
+  const { featured, company, name, sort, fields } = req.query;
 
   const queryObject = {};
   //**If 'featured' exists then set the right value in the 'queryObject' */
@@ -17,11 +17,36 @@ const getAllProducts = async (req, res) => {
     queryObject.featured = featured === "true" ? true : false;
   }
 
-  if(company){
-    queryObject.company = company
+  if (company) {
+    queryObject.company = company;
   }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+
   console.log(queryObject);
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+  if (sort) {
+    console.log(sort);
+    const sortedList = sort.split(",").join(" ");
+    result = result.sort(sortedList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  if (fields) {
+    const sortedFields = fields.split(",").join(" ");
+    result = result.select(sortedFields);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
+  const products = await result;
+  //const products = await Product.find(queryObject);
   res.status(200).json({ products, nbHits: products.length });
 };
 
